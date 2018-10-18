70229fde5ab16ababa82a8375c11c0607e78792e
刘群 <qunliu@zyhx-group.com>
2018-10-18 19:46:24 +0800

    关闭调试选项

--- src/appdef.cpp
+++ src/appdef.cpp
@@ -1,6 +1,5 @@
 #include "appdef.h"
 namespace AppDef {
-const int DEBUG=1;
-const char DEFAULT_MEDIA_SRC[]="rtsp://184.72.239.149/vod/mp4://BigBuckBunny_175k.mov";
-//const char DEFAULT_MEDIA_SRC[]="rtsp://192.168.1.100/user=admin&password=&channel=1&stream=0.sdp?real_stream";
+const int DEBUG=0;
+const char DEFAULT_MEDIA_SRC[]="rtsp://192.168.1.100/user=admin&password=&channel=1&stream=0.sdp?real_stream";
 }
